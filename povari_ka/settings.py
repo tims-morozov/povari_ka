22ffc4f4a4aaa810994e1be928d65d234a0bdaf5
tims-morozov <tims.morozov@gmail.com>
2024-07-26 16:46:16 +0300

    Connect Supabase

--- povari_ka/settings.py
+++ povari_ka/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'povari_ka.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com', 
+        'NAME': 'postgres', 
+        'USER': 'postgres.qvuvfbspssxryvhrswdh', 
+        'PORT': '6543', 
+        'PASSWORD': 'povaritims8', 
+    } 
 }
 
 
